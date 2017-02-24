ae5d4db2664c6d0718b3f5739e03b65e091c3cb8
Jan <jan@Luna.home>
2017-02-24 15:58:53 +0100

    columns can now be added

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-var module = angular.module('helloApp', []);
+var module = angular.module('helloApp', [])
 
-module.controller('helloController', function($scope) {
-  $scope.name = "Gijs";
+module.controller('helloController', function ($scope) {
+  $scope.name = 'Gijs'
 
-});
+  $scope.columns = []
+
+  $scope.saveNewColumn = function () {
+    $scope.columns.push({
+      title: $scope.newColumn.name
+    })
+
+    $scope.newColumn = {}
+  }
+
+  $scope.saveNewColumnItem = function () {
+
+  }
+})
