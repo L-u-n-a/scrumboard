d26337b5598b083d4d11f88713ca87adaa3c5495
Jan <jan@Luna.home>
2017-02-27 12:52:51 +0100

    Tasks can now be assigned to columns.

--- app.js
+++ app.js
@@ -4,17 +4,40 @@ module.controller('helloController', function ($scope) {
 
   $scope.columns = []
 
+  // This is where column information is stored. e.g: ToDo, In Progress, etc.
   $scope.saveNewColumn = function () {
-    if($scope.newColumn.name != null) {
+    if(!isEmpty($scope.newColumn.name)) {
       $scope.columns.push({
-        title: $scope.newColumn.name
+        title: $scope.newColumn.name,
+        // This array will be filled with the tasks in this column.
+        tasks: []
       })
 
       $scope.newColumn = {}
     }
   }
 
-  $scope.saveNewColumnItem = function () {
+  // Save a new item to a column.
+  $scope.saveNewColumnItem = function (column) {
+    if(!isEmpty($scope.newColumn.ItemName) && !isEmpty($scope.newColumn.ItemBody)) {
+      var addItem = -1
+  		$scope.columns.forEach(function(e, i) {
+  			if(e.name === column.name) {
+  				addItem = i;
+  			}
+  		})
 
+      column.tasks.push({
+        name: $scope.newColumn.ItemName,
+        body: $scope.newColumn.ItemBody
+      })
+
+      $scope.newColumn.ItemName = $scope.newColumn.ItemBody = null;
+    }
+  }
+
+  // This function will return true if a value is null or empty.
+  function isEmpty(value) {
+    return (value == null || value.length === 0)
   }
 })
