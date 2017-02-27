b439764549c4aa13e7f02aa527e50fef9a88c3b1
Jan <jan@Luna.home>
2017-02-27 10:11:29 +0100

    added an overlay when adding an item to a column. Column items can't yet be added though.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 var module = angular.module('helloApp', [])
 
 module.controller('helloController', function ($scope) {
-  $scope.name = 'Gijs'
 
   $scope.columns = []
 
   $scope.saveNewColumn = function () {
-    $scope.columns.push({
-      title: $scope.newColumn.name
-    })
+    if($scope.newColumn.name != null) {
+      $scope.columns.push({
+        title: $scope.newColumn.name
+      })
 
-    $scope.newColumn = {}
+      $scope.newColumn = {}
+    }
   }
 
   $scope.saveNewColumnItem = function () {
