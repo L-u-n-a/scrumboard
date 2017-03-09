7655d10cc1051f6b20daf374c844afa01aaec6bd
Jan <jan@Luna.home>
2017-03-09 20:31:10 +0100

    app.js is now written fully to js-standard rules

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
+/* global angular */
 var module = angular.module('helloApp', [])
 
 module.controller('helloController', function ($scope) {
-
   $scope.columns = []
 
   // This is where column information is stored. e.g: ToDo, In Progress, etc.
   $scope.saveNewColumn = function () {
-    if(!isEmpty($scope.newColumn.name)) {
+    if (!isEmpty($scope.newColumn.name)) {
       $scope.columns.push({
         title: $scope.newColumn.name,
         // This array will be filled with the tasks in this column.
@@ -19,25 +19,18 @@ module.controller('helloController', function ($scope) {
 
   // Save a new item to a column.
   $scope.saveNewColumnItem = function (column) {
-    if(!isEmpty($scope.newColumn.ItemName) && !isEmpty($scope.newColumn.ItemBody)) {
-      var addItem = -1
-  		$scope.columns.forEach(function(e, i) {
-  			if(e.name === column.name) {
-  				addItem = i;
-  			}
-  		})
-
+    if (!isEmpty($scope.newColumn.ItemName) && !isEmpty($scope.newColumn.ItemBody)) {
       column.tasks.push({
         name: $scope.newColumn.ItemName,
         body: $scope.newColumn.ItemBody
       })
 
-      $scope.newColumn.ItemName = $scope.newColumn.ItemBody = null;
+      $scope.newColumn.ItemName = $scope.newColumn.ItemBody = null
     }
   }
 
   // This function will return true if a value is null or empty.
-  function isEmpty(value) {
+  function isEmpty (value) {
     return (value == null || value.length === 0)
   }
 })
