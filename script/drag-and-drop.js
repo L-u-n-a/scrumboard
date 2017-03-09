8873e92cedf01e0a26446e8b0277a91e6cb73895
Jan <jan@Luna.home>
2017-03-09 20:22:15 +0100

    Drag and drop now works for tasks

--- script/drag-and-drop.js
+++ script/drag-and-drop.js
@@ -6,18 +6,8 @@ function drag (ev) {
   ev.dataTransfer.setData('text', ev.target.id)
 }
 
-function drop (task) {
-  task.preventDefault()
-  var data = task.dataTransfer.getData('text')
-  task.target.appendChild(document.getElementById(data))
-}
-
-function findColumn (task) {
-  var addItem = -1
-  $scope.column.forEach(function (e, i) {
-    if (e.name === task.name) {
-      addItem = i
-      return addItem
-    }
-  })
+function drop (ev) {
+  ev.preventDefault()
+  var data = ev.dataTransfer.getData('text')
+  ev.target.appendChild(document.getElementById(data))
 }
