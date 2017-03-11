5f2fadaec19784ae790a48e8a88df4fbed6422dc
Jan <jan@Luna.home>
2017-03-11 21:05:56 +0100

    can now filter based on sprint. error message when trying to create item without valid sprint, body or title.

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var module = angular.module('helloApp', [])
 
 module.controller('helloController', function ($scope) {
   $scope.columns = []
+  $scope.sprints = []
+  $scope.warning = ''
 
   // This is where column information is stored. e.g: ToDo, In Progress, etc.
   $scope.saveNewColumn = function () {
@@ -19,14 +21,27 @@ module.controller('helloController', function ($scope) {
 
   // Save a new item to a column.
   $scope.saveNewColumnItem = function (column) {
-    if (!isEmpty($scope.newColumn.ItemName) && !isEmpty($scope.newColumn.ItemBody)) {
+    if (!isEmpty($scope.newColumn.ItemName) && !isEmpty($scope.newColumn.ItemBody) && !isEmpty($scope.newColumn.ItemSprint)) {
       column.tasks.push({
         name: $scope.newColumn.ItemName,
         body: $scope.newColumn.ItemBody,
-        status: null
+        sprint: $scope.newColumn.ItemSprint
       })
+      $scope.warning = ''
 
       $scope.newColumn.ItemName = $scope.newColumn.ItemBody = null
+    } else {
+      $scope.warning = 'Please fill all fields!'
+    }
+  }
+
+  $scope.saveNewSprint = function () {
+    if (!isEmpty($scope.newSprint.name)) {
+      $scope.sprints.push({
+        name: $scope.newSprint.name
+      })
+
+      $scope.newSprint.name = null
     }
   }
 
