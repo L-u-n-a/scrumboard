69143f17c723798e0d4e1379f68aff3b62954462
Jan <jan@Luna.home>
2017-03-31 15:54:30 +0200

    removed some commented code

--- tests/e2e/controllers/loginController.js
+++ tests/e2e/controllers/loginController.js
@@ -8,27 +8,4 @@ describe('E2E: Test the login page.', function () {
     var username = element(by.id('username'))
     expect(username.getText()).toContain('Username')
   })
-
-  // it('should display a different status text after button click', function() {
-  //
-  //   element(by.tagName('button')).click();
-  //
-  //
-  //   expect(element(by.tagName('p')).getText()).toEqual('Done!');
-  // });
-  //
-  // it('should display a hellow message when the form is submitted', function() {
-  //
-  //   element(by.model('name')).sendKeys("Dominic");
-  //
-  //   element(by.css('button#form-submit')).click();
-  //
-  //   expect(element(by.tagName('span')).getText()).toBe("Hellow Dominic");
-  // });
-  //
-  // it('should display a list of todo items', function() {
-  //   var rows = element.all(by.repeater('item in items')).count();
-  //   expect(rows).toBe(3);
-  // });
-
 })
