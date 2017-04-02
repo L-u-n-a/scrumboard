20b160d9fa995a83af452b78227c234644644a13
Jan <jan@Luna.home>
2017-04-02 20:20:57 +0200

    hopefully fixed routing on github pages

--- app.js
+++ app.js
@@ -4,19 +4,19 @@ var app = angular.module('scrumboard-app', ['ngRoute', 'ngCookies'])
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: '/view/login.html',
+      templateUrl: 'view/login.html',
       controller: 'loginController'
     })
     .when('/scrumboard', {
-      templateUrl: '/view/scrumboard.html',
+      templateUrl: 'view/scrumboard.html',
       controller: 'scrumboardController'
     })
     .when('/login', {
-      templateUrl: '/view/login.html',
+      templateUrl: 'view/login.html',
       controller: 'loginController'
     })
     .when('/register', {
-      templateUrl: '/view/register.html',
+      templateUrl: 'view/register.html',
       controller: 'registerController'
     })
     .when('/home', {
