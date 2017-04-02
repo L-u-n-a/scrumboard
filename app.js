84f9694551728f40b54a2474a8cdd455d872ffdf
Jan <jan@Luna.home>
2017-04-02 19:52:37 +0200

    link test

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var app = angular.module('scrumboard-app', ['ngRoute', 'ngCookies'])
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: '/view/home.html',
-      controller: 'homeController'
+      templateUrl: '/view/login.html',
+      controller: 'loginController'
     })
     .when('/scrumboard', {
       templateUrl: '/view/scrumboard.html',
@@ -18,6 +18,10 @@ app.config(function ($routeProvider) {
     .when('/register', {
       templateUrl: '/view/register.html',
       controller: 'registerController'
+    })
+    .when('/home', {
+      templateUrl: 'view/home.html',
+      controller: 'homeController'
     })
     .otherwise({redirectTo: '/'})
 })
