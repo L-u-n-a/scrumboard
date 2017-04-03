fabebb2b68f3800a951f64f38e24e0126a08b4ea
Jan <jan@Luna.home>
2017-04-03 06:12:25 +0200

    projects are now stored right in localstorage

--- factories/projectFactory.js
+++ factories/projectFactory.js
@@ -4,6 +4,10 @@ app.factory('projectFactory', function () {
     {id: 1, name: 'testProject', participants: ['jan'], sprints: []}
   ]
 
+  console.log(projects)
+  projects = JSON.parse(localStorage.getItem('projects'))
+  console.log(projects)
+
   function getAllProjects () {
     return projects
   }
