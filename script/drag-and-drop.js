511802717bde7a71fadb84160cad92c4d4396531
luna <j.vannatijne@gmail.com>
2017-04-22 11:00:39 +0200

    elements can no longer be dropped into each other

--- script/drag-and-drop.js
+++ script/drag-and-drop.js
@@ -9,10 +9,12 @@ function drag (ev) {
   draggedTask = ev.target.id;
 }
 
-function drop (ev) {
+function drop (ev, el) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData('text');
-  ev.target.appendChild(document.getElementById(data));
+
+  // This ensures the task cannot be dropped into another task.
+  el.appendChild(document.getElementById(data));
 
   // This code selects the scope by selecting the div the angular content is
   // beeing displayed in.
@@ -21,4 +23,10 @@ function drop (ev) {
 
   // The ev.target.id is the ID of the column we are dropping the task into.
   scope.switchTaskColumn(draggedTask, parseInt(ev.target.id));
+}
+
+function preventDrop (ev) {
+  if(ev.target.tagName === 'P' || ev.target.tagName === 'A' || ev.target.tagName === 'H4') {
+    ev.preventDefault();
+  }
 }
