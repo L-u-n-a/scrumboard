f2e31cc05560d74cdcd6c725cf26694c65649454
luna <j.vannatijne@gmail.com>
2017-04-22 09:17:20 +0200

    clean up of drag and drop file

--- script/drag-and-drop.js
+++ script/drag-and-drop.js
@@ -1,24 +1,24 @@
 var draggedTask = 0;
 
 function allowDrop (ev) {
-  ev.preventDefault()
-
-  // This selects the ID of the div the item is beeing dropped into.
-  // The id of the div is the same as the ID of the list we want to put it in.
-  // console.log(ev.target.id);
+  ev.preventDefault();
 }
 
 function drag (ev) {
-  ev.dataTransfer.setData('text', ev.target.id)
+  ev.dataTransfer.setData('text', ev.target.id);
   draggedTask = ev.target.id;
 }
 
 function drop (ev) {
-  ev.preventDefault()
-  var data = ev.dataTransfer.getData('text')
-  ev.target.appendChild(document.getElementById(data))
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData('text');
+  ev.target.appendChild(document.getElementById(data));
 
-  // Currently, this fucntion is not within scope, and cannot be used.
+  // This code selects the scope by selecting the div the angular content is
+  // beeing displayed in.
+  /* global angular */
   var scope = angular.element(document.getElementById('content')).scope();
+
+  // The ev.target.id is the ID of the column we are dropping the task into.
   scope.switchTaskColumn(draggedTask, parseInt(ev.target.id));
 }
